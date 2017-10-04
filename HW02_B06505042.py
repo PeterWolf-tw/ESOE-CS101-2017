183735eec1b9dfd74a5f8aaf3cc6a7a00a63ad1d
YuTungLin <32251681+YuTungLin@users.noreply.github.com>
2017-10-04 16:24:40 +0800

    HW02_B06505042.py

--- HW02_B06505042.py
+++ HW02_B06505042.py
@@ -110,16 +110,16 @@ class HW02:
         "xxx" ： 你要填入你的答案在 xxx 這裡。
         '''
         #作業 5. 課本 Ch3. P3.28
-        self.Ch3P3_28a = "-765"
-        self.Ch3P3_28b = "-439"
-        self.Ch3P3_28c = "+874"
-        self.Ch3P3_28d = "+888"
+        self.Ch3P3_28a = "234"
+        self.Ch3P3_28b = "560"
+        self.Ch3P3_28c = "874"
+        self.Ch3P3_28d = "888"
 
         #作業 6. 課本 Ch3. P3.30
-        self.Ch3P3_30a = "-766"
-        self.Ch3P3_30b = "-440"
-        self.Ch3P3_30c = "+875"
-        self.Ch3P3_30d = "+889"
+        self.Ch3P3_30a = "234"
+        self.Ch3P3_30b = "560"
+        self.Ch3P3_30c = "875"
+        self.Ch3P3_30d = "889"
 
 
 if __name__ == '__main__': #程式進入點，程式由此行開始執行。以下示範助教的批改程式。
