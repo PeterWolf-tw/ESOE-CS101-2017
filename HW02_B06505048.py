34b822033302e89b8024b40e9ecbb26ab79c9848
黃日新 <32271734+teamilk@users.noreply.github.com>
2017-10-05 10:47:26 +0800

    Update HW02_B06505048.py

--- HW02_B06505048.py
+++ HW02_B06505048.py
@@ -51,13 +51,13 @@ class HW02:
         "xxx" ： 你要填入你的答案在 xxx 這裡。
         '''
         #作業 5. 課本 Ch3. P3.28
-        self.Ch3P3_28a = ""
-        self.Ch3P3_28b = ""
-        self.Ch3P3_28c = ""
-        self.Ch3P3_28d = ""
+        self.Ch3P3_28a = "765"
+        self.Ch3P3_28b = "439"
+        self.Ch3P3_28c = "125"
+        self.Ch3P3_28d = "111"
 
         #作業 6. 課本 Ch3. P3.30
-        self.Ch3P3_30a = ""
-        self.Ch3P3_30b = ""
-        self.Ch3P3_30c = ""
-        self.Ch3P3_30d = ""
+        self.Ch3P3_30a = "766"
+        self.Ch3P3_30b = "440"
+        self.Ch3P3_30c = "126"
+        self.Ch3P3_30d = "112"
