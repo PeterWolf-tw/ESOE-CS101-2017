b58069bc7c1596da983b2a1d407e5a25940d26d9
YuChuAn129 <erica31129@gmail.com>
2017-10-25 07:43:54 +0800

    HW02_Reviewed

--- HW02_B06505006.py
+++ HW02_B06505006.py
@@ -106,14 +106,14 @@ class HW02:
         "xxx" ： 你要填入你的答案在 xxx 這裡。
         '''
         #作業 5. 課本 Ch3. P3.28
-        self.Ch3P3_28a = "765"
-        self.Ch3P3_28b = "439"
+        self.Ch3P3_28a = "765" #錯誤
+        self.Ch3P3_28b = "439" #錯誤
         self.Ch3P3_28c = "874"
         self.Ch3P3_28d = "888"
 
         #作業 6. 課本 Ch3. P3.30
-        self.Ch3P3_30a = "766"
-        self.Ch3P3_30b = "440"
+        self.Ch3P3_30a = "766" #錯誤
+        self.Ch3P3_30b = "440" #錯誤
         self.Ch3P3_30c = "875"
         self.Ch3P3_30d = "889"
 
