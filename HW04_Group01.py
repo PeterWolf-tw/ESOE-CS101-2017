a1a04ef670fd525e0e9d0f709bb4860a96f26577
teamilk <morris32147896@gmail.com>
2017-12-12 17:50:16 +0800

    Change HW04 #1
    Change HW04 #1

--- HW04_Group01.py
+++ HW04_Group01.py
@@ -15,20 +15,31 @@ crewDICT = {1: {"姓名": "張哲浩",
 import wave
 import struct
 
-sound = wave.open("/Users/harry/Documents/GitHub/ESOE-CS101-2017/ESOE-CS101-2017/44100.wav")
+sound = wave.open('./44100.wav')
 nchannels, sampwidth, framerate, nframes, comptype, compname = sound.getparams()
 
-showAll = False # Show all data in raw string at once.
-if showAll == True:
-    tapeAll = sound.readframes(nframes)
-else:
-    for i in range(0, nframes):
-        waveData = sound.readframes(1)
-        newwaveData = int(waveData)/4
-        newwaveData = bytes(newwaveData)
-        tapeClip = struct.unpack("<h", newwaveData)
-        print(tapeClip)
-        
+box = []
+for i in range(0, nframes):
+    waveData = sound.readframes(1)
+    tapeClip = str(struct.unpack("<h", waveData))
+    x = tapeClip.index(',')
+    tapeClip = int(tapeClip[1:x])
+    box.append(tapeClip)
+
+sound.close()
+
+G1 = wave.open('./Group01.wav', 'w')
+G1.setparams((1, 2, 11025, 110250, 'NONE', 'not compressed'))
+
+t = 0
+while t in range(0, nframes):
+    newsound = struct.pack('h', box[t])
+    G1.writeframes(newsound)
+    t = t + 4
+
+G1.close()
+
+
 # 第二題
 b1 = b"\xa5x\xa4j\xa4u\xac\xec"
 b2 = b"\xe5\x8f\xb0\xe5\xa4\xa7\xe5\xb7\xa5\xe7\xa7\x91"
@@ -58,6 +69,3 @@ print('Ans(c) = htc One M9 Bluetooth 4.1 \n'
       '傳輸時間理論值0.903s (2.71*8/24=0.903s) \n'
       '實際傳輸時間約18.5秒 \n'
       '影響因素:旁邊其他的電子用品、金屬製品、電源、wifi、3G/4G等其他無線區域網路干擾、手機間的距離、考慮資料從記憶卡讀取和存取的速度')
-
-
-
