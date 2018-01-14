45e885f015e86173308a3c785ca3d33802f5084d
momo1106github <31942629+momo1106github@users.noreply.github.com>
2018-01-14 23:37:40 +0800

    HW05_Group05.py

--- HW05_Group03.py
+++ HW05_Group03.py
@@ -4,13 +4,13 @@
 #上繳作業前，請另存一份 "HW05_GroupXX.py"，那個 xx 就是你們的組別編號。 (如果不知道自己組別的，可從 CEIBA 上查詢。)
 #請將組員名單詳列如下，並將範例說明用的「王大錘」及其學號取代為你的組員。若你的組員超過三人，請自行加上。
 crewDICT = {1: {"姓名": "李鼎翊",
-                "學號": "b06505029"},
-            2: {"姓名":"謝心默",
-                "學號":"b06505017"},
-            3: {"姓名":"沈揚",
-                "學號":"b06505018"},
-            4: {"姓名":"韋昊臣",
-                "學號":"b06505031"}
+                "學號": "B06505029"},
+            2: {"姓名": "謝心默",
+                "學號": "B06505017"},
+            3: {"姓名": "沈揚",
+                "學號": "B06505018"},
+            4: {"姓名": "韋昊臣",
+                "學號": "B06505031"}
             }
 
 
@@ -18,3 +18,42 @@ crewDICT = {1: {"姓名": "李鼎翊",
 #並利用 urllib 和 request 兩個套件 (如果你的系統沒有，請自行安裝) 實作抓取
 # http://140.112.27.24:5566/
 #這個網頁中提到的三個頁面。 其中 http://140.112.27.24:5566/add?x=10&y=5 這個連結的 x 和 y 值可自定。
+
+import urllib.request
+
+#方法一
+website = urllib.request.urlopen('http://140.112.27.24:5566/') 
+data = website.read()
+#print(data)
+
+#方法二
+re = urllib.request.Request('http://140.112.27.24:5566/')
+data_1 = urllib.request.urlopen(re)
+data = data_1.read()
+print(data)
+
+
+
+#方法一
+website = urllib.request.urlopen('http://140.112.27.24:5566/add?x=1&y=20') 
+data = website.read()
+#print(str(int(data)))
+
+#方法二
+re = urllib.request.Request('http://140.112.27.24:5566/add?x=1&y=20')
+data_2 = urllib.request.urlopen(re)
+data = data_2.read()
+print(str(int(data)))
+
+
+
+#方法一
+website = urllib.request.urlopen('http://140.112.27.24:5566/page') 
+data = website.read()
+#print(data)
+
+#方法二
+re = urllib.request.Request('http://140.112.27.24:5566/page')
+data_3 = urllib.request.urlopen(re)
+data = data_3.read()
+print(data)
