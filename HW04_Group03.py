95169efa321cd61c2fb0f530dc0ced045f40117e
momo1106github <31942629+momo1106github@users.noreply.github.com>
2018-01-14 23:25:34 +0800

    HW04_Group03.py

--- HW04_Group03.py
+++ HW04_Group03.py
@@ -6,11 +6,11 @@
 crewDICT = {1: {"姓名": "李鼎翊",
                 "學號": "B06505029"},
             2: {"姓名":"謝心默",
-                "學號":"b06505017"},
+                "學號":"B06505017"},
             3: {"姓名":"沈揚",
-                "學號":"b06505018"},
+                "學號":"B06505018"},
             4: {"姓名":"韋昊臣",
-                "學號":"b06505031"}
+                "學號":"B06505031"}
             }
 
 
@@ -18,17 +18,31 @@ crewDICT = {1: {"姓名": "李鼎翊",
 import wave
 import struct
 
-sound = wave.open("./44100.wav")
+sound = wave.open("44100.wav")
 nchannels, sampwidth, framerate, nframes, comptype, compname = sound.getparams()
 
-showAll = True # Show all data in raw string at once.
+showAll = False # Show all data in raw string at once.
 if showAll == True:
     tapeAll = sound.readframes(nframes)
+    #print (tapeAll)
 else:
+    resamp = []
     for i in range(0, nframes):
         waveData = sound.readframes(1)
         tapeClip = struct.unpack("<h", waveData)
-        print(tapeClip)
+        #print(tapeClip)
+        resamp.append(tapeClip[0])
+
+sound.close()
+
+sound_1 = wave.open("11025.wav", "w")
+sound_1.setparams((1, 2, 11025, 110250, 'NONE', 'not compressed'))
+
+for i in range(0,nframes,4):
+    packed_value = struct.pack('h', resamp[i])
+    sound_1.writeframes(packed_value)
+
+sound_1.close()
 
 
 # 第二題：請查詢 Python3 的 decode() 文件，利用 Python3 的 decode() 將以下三個字串轉成中文字串並印出。
@@ -36,33 +50,35 @@ b1 = b"\xa5x\xa4j\xa4u\xac\xec"
 b2 = b"\xe5\x8f\xb0\xe5\xa4\xa7\xe5\xb7\xa5\xe7\xa7\x91"
 b3 = b"\xff\xfe\xf0S'Y\xe5]\xd1y"
 
-print(b1.decode('big5','strict'))
-print(b2.decode('UTF-8','strict'))
-print(b3.decode('utf-16','ignore'))
-
-台大工科
-台大工科
-台大工科
+print (b1.decode("cp950"))
+print (b2.decode("UTF-8"))
+print (b3.decode("UTF-16"))
 
 # 第三題：請查詢 Python3 的 encode() 文件，利用 Python3 的 encode() 將以下的字串轉成 cp950, utf-8 和 utf-16 的編碼。
 s0 = "計算機概論與程式設計"
-s_cp950 = b""
-s_utf8 = b""
-s_utf16 = b""
-print(s0.encode("cp950","strict"))
-print(s0.encode("utf-8","strict"))
-print(s0.encode("utf-16","strict"))
+s_cp950 = s0.encode("cp950")
+s_utf8 = s0.encode("UTF-8")
+s_utf16 = s0.encode("UTF-16")
 
-b'\xadp\xba\xe2\xbe\xf7\xb7\xa7\xbd\xd7\xbbP\xb5{\xa6\xa1\xb3]\xadp'
-b'\xe8\xa8\x88\xe7\xae\x97\xe6\xa9\x9f\xe6\xa6\x82\xe8\xab\x96\xe8\x88\x87\xe7\xa8\x8b\xe5\xbc\x8f\xe8\xa8\xad\xe8\xa8\x88'
-b'\xff\xfe\x08\x8a\x97{_j\x82i\xd6\x8a\x07\x82\x0bz\x0f_-\x8a\x08\x8a'
+print (s_cp950)
+print (s_utf8)
+print (s_utf16)
 
 # 第四題：請說明 Wifi 和 Bluetooth 之間...
 # (a). 哪一種傳輸方式較為耗電？
-藍芽
+
+print("Wifi")
+
 # (b). 哪一種傳輸方式較快速？
+
+print("Wifi")
 
 # (c). 請實際測試：請查出你的手機型號採用的 Bluetooth 規格，再用你的手機拍攝一張照片，
 #      並透過 Bluetooth 傳送該照片到朋友的手機裡。 考量到雙方手機的藍芽設備規格以及照
 #      片的解析度、檔案大小，理論上應該耗時多少時間完成傳送？而實際上又耗了多少時間進行
 #      傳送？ 最後並請列出所有可能影響傳送時間的因素。
+
+print("手機Bluetooth規格：htc desire 816 Bluetooth 4.0")
+print("傳輸速度理論值24Mbps, 照片大小941.22KB, 傳輸時間理論值=(941.22*8)/24000=0.31374秒")
+print("實際耗時:約十一秒")
+print("影響因素:訊號的干擾、收發端距離、手機解讀需時間")
