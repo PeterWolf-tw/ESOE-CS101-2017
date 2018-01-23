145abfa6e9556b7225c6d5e55f08786f5032b861
PeterWolf <peterwolf.wang@gmail.com>
2018-01-23 17:25:56 +0800

    HW01 Reviewed.

--- HW01_b06505019.py
+++ HW01_b06505019.py
@@ -14,23 +14,24 @@
 def modelPrinter(model):
     turingModelSTR = '''Turing Model 是利用機器模仿人類運算數學的過程，包含一條無限長的紙帶、一個讀寫頭、一套控制規則及一個狀態暫存器。
 在讀寫頭讀取紙帶上的訊息(input)後，依目前狀態暫存器內的控制規則控制讀寫頭改變紙帶上的記號(output)，並控制讀寫頭往左或右移動，直到遇到控制規則中的停止訊號。
-    
+
     '''
-    
+
     vonNewmannModelSTR = '''von Neumann Model分為四個部分:記憶體(memory)、算術邏輯單元(arithmetic logic unit)、控制單元(control unit)及輸入與輸出(input/output)。
 與早期電腦最大的不同是除了資料(data)以外，程式(program)也儲存在記憶體內，並且同樣以二進位的方式儲存。
 運作方式則是在控制單元讀取記憶體中程式的指令後解碼並"依照順序"執行。
-    
+
     '''
-    
+    #三個單引號或雙引號就可以換行了。
+    #通常我們會避免寫出左右太「寬」的程式碼。
     if model == "Alen Turing":
         print(turingModelSTR)
     elif model == "von Newmann":
         print(vonNewmannModelSTR)
     else:
         pass
-        
-        
+
+
 if __name__ == "__main__":
     modelPrinter("Alen Turing")
     print("=====我是分隔線=====")
