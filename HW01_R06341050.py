5309c9e3d8afd1ee4eedb33b6b95532fba34974c
PeterWolf <peterwolf.wang@gmail.com>
2018-01-23 17:22:28 +0800

    HW01 Reviewed.

--- HW01_R06341050.py
+++ HW01_R06341050.py
@@ -16,19 +16,20 @@ def modelPrinter(model):
         After reading a message from the tape, the machine translate this message from a "table",  ouput the result based on the current state of machine. The output contains the logic how to modify the current state and the message on the tape.'''
 
 
-    
+
     vonNewmannModelSTR = '''The major difference between Turing model and von Neumann model is that the
         "table" used to translate message is an additional input, that is, the logic
         of how to translate the input is "user-friendly.'''
-    
+
     if model == "Alen Turing":
         print(turingModelSTR)
     elif model == "von Newmann":
         print(vonNewmannModelSTR)
     else:
         pass
-        
-        
+
+
+
 if __name__ == "__main__":
     modelPrinter("Alen Turing")
     print("=====我是分隔線=====")
