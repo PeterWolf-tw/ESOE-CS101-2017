9c5f1ad14315f461157590586e9ad571f88cfc5d
PeterWolf <peterwolf.wang@gmail.com>
2018-01-23 17:17:33 +0800

    HW01 Reviewed.

--- HW01_B06505046.py
+++ HW01_B06505046.py
@@ -16,17 +16,18 @@ def modelPrinter(model):
                         現在有個我們要解決的問題，被記錄成初始狀態。然後我們要解決的方法被寫成一個程式。這個程式會引導讀寫頭的行為
                         讀寫頭可以看現在是0or1然後根據程式的指示
                         往左或往右或不動。改變數字與否。跳到哪個狀態。
-                        最後，當讀寫頭讀到halt時候，這項問題就被解決了並且最後紙帶上的樣子就是答案。'''    
+                        最後，當讀寫頭讀到halt時候，這項問題就被解決了並且最後紙帶上的樣子就是答案。'''
     vonNewmannModelSTR = '''其實就是在一個機器儲存很多程式然後根據不同的目標，來使用想用的程式。剩下的運作，就是依照上述的圖靈機了吧'''
-    
+
     if model == "Alen Turing":
         print(turingModelSTR)
     elif model == "von Newmann":
         print(vonNewmannModelSTR)
     else:
         pass
-        
-        
+
+
+
 if __name__ == "__main__":
     modelPrinter("Alen Turing")
     print("=====我是分隔線=====")
