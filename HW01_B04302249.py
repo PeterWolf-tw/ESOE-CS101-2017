ba9dbf9d9388e077b51ad2d9301227e88d03cb94
PeterWolf <peterwolf.wang@gmail.com>
2018-01-23 10:50:23 +0800

    HW01 Reviewed.

--- HW01_B04302249.py
+++ HW01_B04302249.py
@@ -16,22 +16,23 @@ def modelPrinter(model):
                         而為了要達成這兩個步驟，他假想出一部機器即turingmachine，在這個機器當中，他有一條無限長的紙帶，紙帶被切割成很多格子，每個格子裡面只能
                         寫下一個符號，然後他有一個讀寫頭，這個讀寫頭只會停留在紙帶上的一個格子上，他可以讀入和改變當前格子中的符號，並可以任意向左右移動，還需要有
                         一套program用來控制讀寫頭的動作，program必須包含initial state和halting state。
-    
+
     '''
-    
+
     vonNewmannModelSTR = '''vonneumannModel 是將電腦分成4個子系統，分別是memory，arithmetic logic unit，control unit，input/output，然後將電腦的
                             program和data都寫入memory，因此program在vonneumannModel下就可以被更改，因此概念上更接近universal turing machine
-    
+
     '''
-    
+
     if model == "Alen Turing":
         print(turingModelSTR)
     elif model == "von Newmann":
         print(vonNewmannModelSTR)
     else:
         pass
-        
-        
+
+
+
 if __name__ == "__main__":
     modelPrinter("Alen Turing")
     print("=====我是分隔線=====")
