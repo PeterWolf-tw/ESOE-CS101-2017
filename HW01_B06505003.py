f13dd9077ea66a5a43889154af05b476b99f61b5
PeterWolf <peterwolf.wang@gmail.com>
2018-01-23 10:57:37 +0800

    HW01 Reviewed.

--- HW01_B06505003.py
+++ HW01_B06505003.py
@@ -17,9 +17,10 @@ def modelPrinter(model):
                     是答案。而讀寫頭則是扮演輸入/輸出的角色。讀寫頭會讀取它所在的格子上之符號，並將其傳送至規則，根據規則的敘述，它會改寫格子內的
                     值，並移到下一個特定的格子。不斷重複直到規則回傳終止。規則是整套演算法的邏輯依據。規則必須要詳述在每一種狀況，讀寫頭讀進任一值
                     ，讀寫頭要如何改寫並且移動到哪一格，並且必須包括結束的條件。根據不同的問題，我們可以設計不同的規則來解決。'''
+
     vonNewmannModelSTR = '''Von Neumann model 是Universal Turing Machine的一種應用。他與一般Turing machine 最大的不同在於它將program和data都
                          儲存在memory中，而兩者都可以作為input 和output。因此只要輸入各種程式，Von Neumann model 便可做多種的用途，符合
-                         Universal Turing Machine的宗旨。它的構成分成5個部分:1.input 2.memory 3.control unit 4.arithmetic logic unit 
+                         Universal Turing Machine的宗旨。它的構成分成5個部分:1.input 2.memory 3.control unit 4.arithmetic logic unit
                          5.output。input代表輸入，它可以是data,也可以是program。Input 輸入後會被儲存到memory中。而control unit則是Von Neumann
                          model中的大腦，它會到memory中讀取其中的program後解碼並讀取其描述的指令，接著它會分配工作給其他的原件來完成指令，執行完畢
                          後再將結果回傳到memory 儲存，並且輸出至output。需要注意的一點是control unit 在執行program 的指令時會按照「依序執行」的
@@ -31,8 +32,8 @@ def modelPrinter(model):
         print(vonNewmannModelSTR)
     else:
         pass
-        
-        
+
+
 if __name__ == "__main__":
     modelPrinter("Alen Turing")
     print("=====我是分隔線=====")
