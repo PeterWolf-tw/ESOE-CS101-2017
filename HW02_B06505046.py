e8140237ef3ea674097885b731700a2947c993a7
PeterWolf <peterwolf.wang@gmail.com>
2018-01-24 10:36:49 +0800

    HW02 Reviewed.

--- HW02_B06505046.py
+++ HW02_B06505046.py
@@ -1,41 +1,46 @@
-#§@·~ 1
-N=input("¿é¤J¤Q¶i¦ì")
-L= int(len(str(N)))      
+#ï¿½@ï¿½~ 1
+
+#Please use an editor/IDE that supports unicode.
+
+N=input("ï¿½ï¿½Jï¿½Qï¿½iï¿½ï¿½")
+L= int(len(str(N)))
     A = 0
      K = 0
-     while L > K :         
-     r = int(N%10)     
-     A = A + (2**K)*r  
-     N = N/10          
-     K = K + 1         
+     while L > K :
+     r = int(N%10)
+     A = A + (2**K)*r
+     N = N/10
+     K = K + 1
         return A
-    str(A)                
+    str(A)
    print('A')
-   #§@·~ 2. ½Ò¥» Ch2. P2.19
+   #ï¿½@ï¿½~ 2. ï¿½Ò¥ï¿½ Ch2. P2.19
    self.Ch2P2_19a = "10"
    self.Ch2P2_19b = "17"
    self.Ch2P2_19c = "6"
    self.Ch2P2_19d = "8"
 
-   #§@·~ 3. ½Ò¥» Ch2. P2.20
+   #ï¿½@ï¿½~ 3. ï¿½Ò¥ï¿½ Ch2. P2.20
    self.Ch2P2_20a = "14"
    self.Ch2P2_20b = "8"
    self.Ch2P2_20c = "13"
    self.Ch2P2_20d = "4"
 
-   #§@·~ 4. ½Ò¥» Ch2. P2.22
+   #ï¿½@ï¿½~ 4. ï¿½Ò¥ï¿½ Ch2. P2.22
    self.Ch2P2_22a = "00010001 11101010 00100010 00001110"
    self.Ch2P2_22b = "01101110 00001110 00111000 01001110"
    self.Ch2P2_22c = "00001110 00111000 11101010 00111000"
    self.Ch2P2_22d = "00011000 00111000 00001101 00001011"
-   #§@·~ 5. ½Ò¥» Ch3. P3.28
+   #ï¿½@ï¿½~ 5. ï¿½Ò¥ï¿½ Ch3. P3.28
        self.Ch3P3_28a = "234"
        self.Ch3P3_28b = "560"
        self.Ch3P3_28c = "874"
        self.Ch3P3_28d = "888"
-   
-       #§@·~ 6. ½Ò¥» Ch3. P3.30
+
+       #ï¿½@ï¿½~ 6. ï¿½Ò¥ï¿½ Ch3. P3.30
        self.Ch3P3_30a = "234"
        self.Ch3P3_30b = "560"
        self.Ch3P3_30c = "875"
-       self.Ch3P3_30d = "889"   
+       self.Ch3P3_30d = "889"
+
+#This program is not executable.
